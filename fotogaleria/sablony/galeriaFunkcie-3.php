31c926810210561be80b6b7d05e9dfc3ab3cae89
OndrejVrto <32365016+OndrejVrto@users.noreply.github.com>
2017-10-03 00:12:19 +0200

    uprava bootstrap.css pre galeriu

--- fotogaleria/sablony/galeriaFunkcie-3.php
+++ fotogaleria/sablony/galeriaFunkcie-3.php
@@ -1,47 +1,3 @@
-<!--			<div class="card-columns">
-				<div class="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-3 px-0">
-					<div class="card mx-2 my-3 border-primary">
-						<a href="/fotogaleria/2015.php?album=2015-12-xx-Zdobenie-stromceka">
-						<img class="card-img-top" src="/_fotoalbumy/2008/2015-12-26%20Dychovka%20na%20Stefana/thumbs/Dychovka2015%20001.jpg" alt="2015-12-xx-Zdobenie-stromceka-Random"/>
-						</a>
-						<div class="card-body">
-							<h4 class="card-title">Na sv. Štefanna pred farským kostolom</h4>
-							<h6 class="card-subtitle mb-2 text-muted">26.12.2015</h6>
-							<p class="card-text">Na sv. Štefanna zahrala dychovka všetkým farnikom. Spev, slovo veselé bolo cítiť na každom kroku.</p>
-							<p class="card-text"><small class="text-muted">Fotila: Marka Ostrihoňová</small></p>
-						</div>
-					</div>
-				</div>
-				<div class="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-3 px-0">
-					<a href="/fotogaleria/2015.php?album=2015-12-xx-Zdobenie-stromceka">
-					<div class="card mx-2 my-3 border-primary">
-						<img class="card-img-top" src="/_fotoalbumy/2008/2015-12-26%20Dychovka%20na%20Stefana/thumbs/Dychovka2015%20010.jpg" alt="2015-12-xx-Zdobenie-stromceka-Random"/>
-						<div class="card-body">
-							<h4 class="card-title">Na sv. Štefanna pred farským kostolom</h4>
-							<h6 class="card-subtitle mb-2 text-muted">26.12.2015</h6>
-							<p class="card-text">Na sv. Štefanna zahrala dychovka všetkým farnikom. Spev, slovo veselé bolo cítiť na každom kroku.</p>
-							<p class="card-text"><small class="text-muted">Fotila: Marka Ostrihoňová</small></p>
-						</div>
-					</div>
-					</a>
-				</div>
-				<div class="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-3 px-0">
-					<div class="card mx-2 my-3 border-primary">
-						<a href="/fotogaleria/2015.php?album=2015-12-xx-Zdobenie-stromceka">
-						<img class="card-img-top" src="/_fotoalbumy/2008/2015-12-26%20Dychovka%20na%20Stefana/thumbs/Dychovka2015%20004.jpg" alt="2015-12-xx-Zdobenie-stromceka-Random"/>
-						<div class="card-body">
-							<h4 class="card-title">Na sv. Štefanna pred farským kostolom</h4>
-							<h6 class="card-subtitle mb-2 text-muted">26.12.2015</h6>
-							<p class="card-text">Na sv. Štefanna zahrala dychovka všetkým farnikom. Spev, slovo veselé bolo cítiť na každom kroku.</p>
-							<p class="card-text"><small class="text-muted">Fotila: Marka Ostrihoňová</small></p>
-						</div>
-						</a>
-					</div>
-				</div>
-			</div>
-			
-			-->
-
 			<div class="row">
 				<div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
 					<nav class="d-flex justify-content-center" aria-label="Page navigation example">
@@ -55,10 +11,8 @@
 				</div>
 			</div>
 			
-			<div class="clearfix"></div>
-			
 			<div class="card-columns">
-				<a href="/fotogaleria/2015.php?album=2015-12-xx-Zdobenie-stromceka">
+				<a href="/fotogaleria/2015/2015-12-xx-Zdobenie-stromceka/1/">
 					<div class="card mx-3 mx-sm-0 border-primary">
 						<img class="card-img-top" src="/_fotoalbumy/2008/2015-12-26%20Dychovka%20na%20Stefana/thumbs/Dychovka2015%20002.jpg" alt="2015-12-xx-Zdobenie-stromceka-Random"/>
 						<div class="card-body">
@@ -69,7 +23,7 @@
 						</div>
 					</div>
 				</a>
-				<a href="/fotogaleria/2015.php?album=2015-12-xx-Zdobenie-stromceka">
+				<a href="/fotogaleria/2015/2015-12-xx-Zdobenie-stromceka/1/">
 					<div class="card mx-3 mx-sm-0 border-primary">
 						<img class="card-img-top" src="/_fotoalbumy/2008/2015-12-26%20Dychovka%20na%20Stefana/thumbs/Dychovka2015%20010.jpg" alt="2015-12-xx-Zdobenie-stromceka-Random"/>
 						<div class="card-body">
@@ -80,7 +34,7 @@
 						</div>
 					</div>
 				</a>
-				<a href="/fotogaleria/2015.php?album=2015-12-xx-Zdobenie-stromceka">
+				<a href="/fotogaleria/2015/2015-12-xx-Zdobenie-stromceka/1/">
 					<div class="card mx-3 mx-sm-0 border-primary">
 						<img class="card-img-top" src="/_fotoalbumy/2008/2015-12-26%20Dychovka%20na%20Stefana/thumbs/Dychovka2015%20004.jpg" alt="2015-12-xx-Zdobenie-stromceka-Random"/>
 						<div class="card-body">
@@ -91,7 +45,7 @@
 						</div>
 					</div>
 				</a>
-				<a href="/fotogaleria/2015.php?album=2015-12-xx-Zdobenie-stromceka">
+				<a href="/fotogaleria/2015/2015-12-xx-Zdobenie-stromceka/1/">
 					<div class="card mx-3 mx-sm-0 border-primary">
 						<img class="card-img-top" src="/_fotoalbumy/2008/2015-12-26%20Dychovka%20na%20Stefana/thumbs/Dychovka2015%20003.jpg" alt="2015-12-xx-Zdobenie-stromceka-Random"/>
 						<div class="card-body">
@@ -102,9 +56,9 @@
 						</div>
 					</div>
 				</a>
-				<a href="/fotogaleria/2015.php?album=2015-12-xx-Zdobenie-stromceka">
+				<a href="/fotogaleria/2015/2015-12-xx-Zdobenie-stromceka/1/">
 					<div class="card mx-3 mx-sm-0 border-primary">
-						<img class="card-img-top" src="/_fotoalbumy/2008/2015-12-26%20Dychovka%20na%20Stefana/thumbs/Dychovka2015%20004.jpg" alt="2015-12-xx-Zdobenie-stromceka-Random"/>
+						<img class="card-img-top" src="/_fotoalbumy/2015/2015-12-xx%20Adventna%20cesta/thumbs/AdvCesta001.jpg" alt="2015-12-xx-Zdobenie-stromceka-Random"/>
 						<div class="card-body">
 							<h4 class="card-title">Na sv. Štefanna pred farským kostolom</h4>
 							<h6 class="card-subtitle mb-2 text-muted">26.12.2015</h6>
@@ -113,7 +67,7 @@
 						</div>
 					</div>
 				</a>
-				<a href="/fotogaleria/2015.php?album=2015-12-xx-Zdobenie-stromceka">
+				<a href="/fotogaleria/2015/2015-12-xx-Zdobenie-stromceka/1/">
 					<div class="card mx-3 mx-sm-0 border-primary">
 						<img class="card-img-top" src="/_fotoalbumy/2008/2015-12-26%20Dychovka%20na%20Stefana/thumbs/Dychovka2015%20005.jpg" alt="2015-12-xx-Zdobenie-stromceka-Random"/>
 						<div class="card-body">
@@ -124,9 +78,9 @@
 						</div>
 					</div>
 				</a>
-				<a href="/fotogaleria/2015.php?album=2015-12-xx-Zdobenie-stromceka">
+				<a href="/fotogaleria/2015/2015-12-xx-Zdobenie-stromceka/1/">
 					<div class="card mx-3 mx-sm-0 border-primary">
-						<img class="card-img-top" src="/_fotoalbumy/2008/2015-12-26%20Dychovka%20na%20Stefana/thumbs/Dychovka2015%20006.jpg" alt="2015-12-xx-Zdobenie-stromceka-Random"/>
+						<img class="card-img-top" src="/_fotoalbumy/2015/2015-12-xx%20Adventna%20cesta/thumbs/AdvCesta010.jpg" alt="2015-12-xx-Zdobenie-stromceka-Random"/>
 						<div class="card-body">
 							<h4 class="card-title">Na sv. Štefanna pred farským kostolom</h4>
 							<h6 class="card-subtitle mb-2 text-muted">26.12.2015</h6>
@@ -135,7 +89,7 @@
 						</div>
 					</div>
 				</a>
-				<a href="/fotogaleria/2015.php?album=2015-12-xx-Zdobenie-stromceka">
+				<a href="/fotogaleria/2015/2015-12-xx-Zdobenie-stromceka/1/">
 					<div class="card mx-3 mx-sm-0 border-primary">
 						<img class="card-img-top" src="/_fotoalbumy/2008/2015-12-26%20Dychovka%20na%20Stefana/thumbs/Dychovka2015%20007.jpg" alt="2015-12-xx-Zdobenie-stromceka-Random"/>
 						<div class="card-body">
@@ -149,12 +103,10 @@
 			
 			</div>
 			
-			<div class="clearfix"></div>
-			
 			<div class="row">
 				<div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
 					<nav class="d-flex justify-content-center" aria-label="Page navigation example">
-						<ul class="pagination">
+						<ul class="pagination" id="druhe">
 							<li class="page-item disabled"><a class="page-link" href="#" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>
 							<li class="page-item active"><a class="page-link" href="/fotogaleria/2008/1/">1<span class="sr-only">(aktívna)</span></a></li>
 							<li class="page-item"><a class="page-link" href="/fotogaleria/2008/2/">2</a></li>
