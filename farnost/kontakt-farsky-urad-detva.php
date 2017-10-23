fcb9ebfc3f5026bd7e0d9a37c67fdc7576071acb
OndrejVrto <32365016+OndrejVrto@users.noreply.github.com>
2017-10-23 17:03:27 +0200

    oprava zobrazovania mapy v kontaktoch

--- farnost/kontakt-farsky-urad-detva.php
+++ farnost/kontakt-farsky-urad-detva.php
@@ -61,7 +61,8 @@
 						<i class="fa fa-globe" aria-hidden="true"></i><br>
 						<a href="/">detva.fara.sk</a>
 					</address>
-					<iframe rel="nofollow" class="mapyGoogle" src="http://maps.google.sk/maps?q=48.559215,19.418877&amp;num=1&amp;t=h&amp;ie=UTF8&amp;ll=48.559215,19.418877&amp;spn=0.002473,0.00456&amp;z=18&amp;output=embed"></iframe>
+					<iframe rel="nofollow" class="embed-responsive rounded mapyGoogle mx-auto" 
+							  src="http://maps.google.sk/maps?q=48.559215,19.418877&amp;num=1&amp;t=h&amp;ie=UTF8&amp;ll=48.559215,19.418877&amp;spn=0.002473,0.00456&amp;z=18&amp;output=embed"></iframe>
 				</div>
 			</div>
 			<div class="well text-center">
@@ -79,7 +80,8 @@
 						<i class="fa fa-globe" aria-hidden="true"></i><br>
 						<a rel="external" target="_blank" href="http://www.sestrydetva.bosikarmelitani.sk">sestrydetva.bosikarmelitani.sk</a>
 					</address>
-					<iframe rel="nofollow" class="mapyGoogle" src="http://maps.google.sk/maps?q=48.547789,19.401817&amp;num=1&amp;t=h&amp;ie=UTF8&amp;ll=48.547789,19.401817&amp;spn=0.002473,0.00456&amp;z=18&amp;output=embed"></iframe>
+					<iframe rel="nofollow" class="embed-responsive mapyGoogle" 
+							  src="http://maps.google.sk/maps?q=48.547789,19.401817&amp;num=1&amp;t=h&amp;ie=UTF8&amp;ll=48.547789,19.401817&amp;spn=0.002473,0.00456&amp;z=18&amp;output=embed"></iframe>
 				</div>
 			</div>
 
