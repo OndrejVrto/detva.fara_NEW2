cd9fb07f2dd3e650d5ad64daa589948421580ba7
OndrejVrto <32365016+OndrejVrto@users.noreply.github.com>
2019-11-14 16:22:51 +0100

    Zoznam vsetkych galerii
    treba este dorobit XML

--- fotogaleria/fotogaleria.php
+++ fotogaleria/fotogaleria.php
@@ -34,7 +34,7 @@ echo "\t\t\t</div>\n";
 
 
 // zmaz tento docasny kod pre vlozenie VZORov
-		echo "\n\n\t<!-- VZOR -->";
+/* 		echo "\n\n\t<!-- VZOR -->";
  		echo "\n\t<h4><i class=\"fa fa-arrow-down\" aria-hidden=\"true\"></i> VZOR <i class=\"fa fa-arrow-down \" aria-hidden=\"true\"></i></h4>";
 		echo "\n\n\t\t<div class=\"gallery\" role=\"img\">\n";
 		if ($nazovGalerie=='zoznam-galerii'){
@@ -46,7 +46,7 @@ echo "\t\t\t</div>\n";
 				include "vzory/jeden-album-VZOR.php";
 			}
 		} 
-		echo "\n\t\t</div>\n";
+		echo "\n\t\t</div>\n"; */
 // zmaz tento docasny kod pre vlozenie VZORov 
 
 include_once $path . "/_vlozene/spodok-stranky.php"; echo "\n";
--- fotogaleria/sablony/zoznam-galerii.php
+++ fotogaleria/sablony/zoznam-galerii.php
@@ -1,16 +1,82 @@
 <?php
 
-/* 	$pracovny = '';
+	$path = $_SERVER['DOCUMENT_ROOT'];
+
+	$pracovny = '';
 	$adresarVSTUP = $adresarFotogaleria;
 	$adresarRELscript = $polohaSkriptu . $adresarVSTUP;
 	$adresarABS = folder_exist($adresarRELscript);
+	$zoznam_Adresarov = glob($adresarABS . '*', GLOB_ONLYDIR);
+	$pocetAdresarov = count($zoznam_Adresarov);
 	
-	$pracovny .= "<ol class=\"mb-0\">";
-	foreach(glob($adresarABS . '*', GLOB_ONLYDIR) as $zoznam_adresarov_Galeria) {
-		 $zoznam_adresarov_Galeria = str_replace($adresarABS, '', $zoznam_adresarov_Galeria);
-		 $pracovny .= "<li>" . $zoznam_adresarov_Galeria. "</li>\n";
-	}
-	$pracovny .= "</ol>";
+	if( $pocetAdresarov == 0 ) {
+		$vystupHTML = 'Na stránke v tejto chvíli nieje pridaná žiadna galéria fotiek.';
+	} else {
+			$zalomenie = "\n\t\t\t\t";
+			$vystupHTML .=	$zalomenie . '<div class="card-columns mb-n3">';
+		
+		foreach($zoznam_Adresarov as $zoznam_Galerii) {
+			
+			$zoznam_Galerii = str_replace($adresarABS, '', $zoznam_Galerii);
 
-$vystupHTML .= $pracovny; */
+			$vystupHTML .= $zalomenie . "\t" . "<div class=\"card mx-sm-0 border-primary\">";
+			
+			$ObrazokAlbumu = get_files($path . $adresarFotogaleria . $zoznam_Galerii);
+			
+			if (!empty($ObrazokAlbumu)){
+				$vystupHTML .= $zalomenie . "\t\t" . "<a href=\"" . $adresaFotogalerieHTML . $zoznam_Galerii . "/1/\">";
+				$vystupHTML .= $zalomenie . "\t\t\t" . "<img class=\"card-img-top\" src=\"" . $adresarFotogaleria . $zoznam_Galerii;
+				$vystupHTML .= "/" . $ObrazokAlbumu[0] . "\" alt=\"Vybraný obrázok z galérie " . $zoznam_Galerii . "\"/>";
+				$vystupHTML .= $zalomenie . "\t\t" . "</a>";		
+			}
+
+			$adresarRELscript2 = $path . $adresarFotogaleria . $zoznam_Galerii;
+			$adresarABS2 = folder_exist($adresarRELscript2);
+			$zoznam_Adresarov2 = glob($adresarABS2 . '*', GLOB_ONLYDIR);
+			$albumovVgalerii = count($zoznam_Adresarov2);
+
+			
+			$vystupHTML .= $zalomenie . "\t\t" . "<div class=\"card-body\">";
+			
+			$vystupHTML .= $zalomenie . "\t\t\t" . "<a href=\"" . $adresaFotogalerieHTML . $zoznam_Galerii . "/1/\">";
+			$vystupHTML .= $zalomenie . "\t\t\t\t" . "<h5 class=\"card-title text-center\">" . $zoznam_Galerii . "</h5>";
+			$vystupHTML .= $zalomenie . "\t\t\t\t" . "<h6 class=\"card-subtitle text-center mb-2 text-muted\">" . $albumovVgalerii . GramatikaAlbumy($albumovVgalerii) . "</h6>";
+			$vystupHTML .= $zalomenie . "\t\t\t" . "</a>";	
+
+			
+			if ($albumovVgalerii!==0){
+				$vystupHTML .= $zalomenie ."\t\t\t" . "<ol class=\"card-text pl-4\">";
+				foreach($zoznam_Adresarov2 as $zoznam_Albumov_Galerii) {
+				
+					$XMLfilename = nazov_suboru($zoznam_Albumov_Galerii);
+
+					if (file_exists($XMLfilename)) {
+						$xml = new DOMDocument();
+						$xml->load( $XMLfilename );
+						$titleALBUM = $xml->getElementsByTagName( "NazovAlbumu" )->item(0)->nodeValue;
+						
+						$vystupHTML .= $zalomenie . "\t\t\t\t" . "<li class=\"vsetkyGalerie\" >" . "<a href=\"" . $xml->getElementsByTagName( "LinkRewrite" )->item(0)->nodeValue . "/1/\">";
+						$vystupHTML .= $titleALBUM . "</a>". "</li>";;
+					
+					} else {
+						$path2 = array_filter(explode('/', $zoznam_Albumov_Galerii));
+						$titleALBUM = $path2[end(array_keys($path2))];
+						$vystupHTML .= $zalomenie ."\t\t\t" . "<li>" . $titleALBUM . "</li>";
+					}
+				}
+				$vystupHTML .= $zalomenie ."\t\t\t" . "</ol>";
+			}
+			$vystupHTML .= $zalomenie ."\t\t" . "</div>";
+			$vystupHTML .= $zalomenie ."\t" . "</div>";
+
+		}
+
+
+			$vystupHTML .=	$zalomenie . '</div>' . "\n";
+	}
+
+
+
+
+
 ?>
